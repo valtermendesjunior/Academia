19842c0674fe32bd2122545890f162014a2c294f
valter mendes junior <mendesselvavalter1@hotmail.com>
2023-10-16 12:00:43 -0300

    init

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academia</title>
+    <link href="assets/css/main.css" rel="stylesheet">
+    </head>
+
+<body>
+
+<?php
+
+ 
+
+     include_once("assets/componentes/cabecalho.html");
+
+ 
+
+ ?>
+
+ 
+
+ <div id="banner">
+
+ 
+
+     <img src="" alt="">
+
+ 
+
+ </div>
+
+ 
+
+ <h1>Sua saúde em movimento!</h1>
+
+ 
+
+ <p>Bem-vindos à Akademia - o local onde seus objetivos de condicionamento físico se transformam em realidade! Somos uma academia de ginástica apaixonada por promover uma vida mais saudável e ativa para todos os nossos membros. Aqui na Akademia, você encontrará um ambiente acolhedor e motivador, repleto de equipamentos modernos, aulas desafiadoras e instrutores altamente qualificados. Independentemente do seu nível de aptidão ou idade, nossa missão é inspirá-lo a alcançar o melhor de si mesmo, oferecendo programas personalizados que se adaptam às suas necessidades individuais. Junte-se a nós e embarque nessa jornada emocionante em direção a um corpo mais forte, uma mente mais clara e uma vida cheia de energia e vitalidade. Seja bem-vindo à Akademia, onde o bem-estar se torna uma paixão compartilhada!</p>
+
+ 
+
+ <selection>
+
+ 
+
+     <h2>Modalidades em destaque</h2>
+
+ 
+
+ </selection>
+
+ 
+
+ <div class="class-container">
+
+ 
+
+     <img src="" alt="">
+
+ 
+
+     <h3>Musculação</h3>
+
+ 
+
+ </div>
+
+ 
+
+ <div class="">
+
+     <img src="" alt="">
+
+     <h3>Ginástica</h3>
+
+ </div>
+
+ 
+
+ <div class="">
+
+     <img src="" alt="">
+
+     <h3>Yoga</h3>
+
+ </div>
+
+ 
+
+ <?php
+
+     include_once("assets/componentes/rodape.html");
+
+ ?>
+
+</body>
+
+</html>
